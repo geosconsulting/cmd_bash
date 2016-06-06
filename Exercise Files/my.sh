7c0c7e383f91aa6627b893a186caa9c53791fa68
Fabio Lana <geos-consulting@fastwebnet.it>
2016-06-06 07:39:13 -0700

    iniziale

---
+++ Exercise Files/my.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+a=Hello
+b="Good Morning"
+c=16
+
+echo $a
+echo $b
+echo $c 
+
+echo "$b! I have $c apples." 
+
+echo $0
+echo $HOME
+echo $PWD
+echo $MACHTYPE
+echo $HOSTNAME
+echo $BASH_VERSION
+
+d=$(pwd)
+echo $d
+
+d1=pwd
+echo $d1
+
+a=$(ping -c 1 example.com |grep 'bytes from'| cut -d = -f 4)
+echo "The ping was $a"
+
+k=2
+e=$((k+10))
+echo $e
+((e++))
+echo $e
---
+++ unpack2/Exercise Files/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -e "\t\t\e[92mHello from the Test Script!\e[39m"
+#line
+#line1
